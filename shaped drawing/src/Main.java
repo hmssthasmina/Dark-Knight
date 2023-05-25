f0558c8585e5748572785bcd77564cd1dfb70f20
hmssthasmina <132302101+hmssthasmina@users.noreply.github.com>
2023-05-25 16:33:25 +0530

    main java shaped drawing

---
+++ shaped drawing/src/Main.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Shape application");
+    }
+}
